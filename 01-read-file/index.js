bbe3637343771154045480020f862bf2bf07220f
Miraslau <36743766+MIROSLAVSH@users.noreply.github.com>
2022-05-23 21:55:16 +0300

    docs: grgr

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 fs.readFile("text.txt", "utf8", 
             function(error,data){
                 console.log("Асинхронное чтение файла");
-                if(error) throw error; // если возникла ошибка
-                console.log(data);  // выводим считанные данные
+                if(error) throw error;
+                console.log(data); 
 });
